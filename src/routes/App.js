fb9b95cee70f5ffcaee372f1c756b256aedfd752
Mauricio Londoño Cianci <chicholoncia@gmail.com>
2021-05-28 00:08:34 -0500

    Interfaz de Login y de Registro hechas

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,23 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Login from "../containers/login/Login";
+import Register from "../containers/login/Register";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from 'react-router-dom';
+
   function App() {
     return (
-      <div>
-          <h1>hola</h1>
-      </div>
+      <ChakraProvider>
+          <Router>
+            <Switch>
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/registro' component={Register} />
+            </Switch>
+          </Router>
+      </ChakraProvider>
     );
   }
   
