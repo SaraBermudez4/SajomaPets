98b8c0c74c94b40188cbf236c7085f9b79d61d6b
Jonathan Cepeda <jonisjfcp@gmail.com>
2021-06-09 21:07:41 -0500

    segundo intento de merge

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import CarouselSwipeableTextMobileStepper from '../../components/home/CarouselSwipeableTextMobileStepper';
+import styled from 'styled-components'
+import TabScrollButton from '../../components/home/TabScrollButton';
 import NabBarMiniVariantDrawer from '../sideBar/NabBarMiniVariantDrawer';
+
+const StyledDivTabs = styled.div`
+    display:flex; 
+    justify-content:center;
+    margin-right: 1px;  
+    margin-left: 72px;
+
+    @media (min-width: 320px) and (max-width: 480px) {
+        margin-left: 58px;
+    }
+`
 
 const Home = () => {
 
     return (
         <>
             <CarouselSwipeableTextMobileStepper />
+            <StyledDivTabs>
+                <TabScrollButton />
             <NabBarMiniVariantDrawer />
+            </StyledDivTabs>
         </>
+        
     )
 }
 
