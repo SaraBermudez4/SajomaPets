0dc35033ab4183072dc3f88f40f749f31c4ce3be
Mauricio Londoño Cianci <chicholoncia@gmail.com>
2021-06-15 23:04:13 -0500

    Finalizando perfil de usuario

--- src/components/profile/DatesProfile.jsx
+++ src/components/profile/DatesProfile.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Accordion, AccordionDetails, AccordionSummary, Box, Button, FormControl, Grid, Typography } from '@material-ui/core';
 
@@ -14,6 +14,8 @@ import { MdAddCircle } from 'react-icons/md'
 import { BsHouseDoor, BsBuilding } from 'react-icons/bs'
 import CreditCardModal from './CreditCardModal';
 import { Modal } from '@chakra-ui/react';
+import { startSaveUser } from '../../actions/userAction';
+import AddressModal from './AddressModal';
 
 const StyledAccordionDetails = styled(AccordionDetails)`
     flex-direction: column;
@@ -51,21 +53,81 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+let modalName = ''
 
 const DatesProfile = () => {
     const classes = useStyles();
 
+    const dispatch = useDispatch()
+
+    const [aInput, setAInput] = useState(false)
+
     const { userData } = useSelector(state => state.user)
     const cards = userData[0].cards
 
-    console.log(cards);
-
     const address = userData[0].addresses
-    
+
+    console.log(address);
+
     const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const initialRef = React.useRef()
-    const finalRef = React.useRef()
+    const initialRef = useRef()
+    const finalRef = useRef()
+
+    const newName = useRef()
+    const newLastName = useRef()
+    const newDocument = useRef()
+    const newPhoneNumber = useRef()
+    const newPhoneNumber2 = useRef()
+
+    const editUser = async (e) => {
+
+        const newNName = newName.current.value
+        const newNLastName = newLastName.current.value
+        const newNDocument = newDocument.current.value
+        const newNPhoneNumber = newPhoneNumber.current.value
+        const newNPhoneNumber2 = newPhoneNumber2.current.value
+
+        const dataUser = {
+            id: userData[0].id,
+            document: newNDocument,
+            lastName: newNLastName,
+            cellPhone: newNPhoneNumber2,
+            nickName: userData[0].nickName,
+            email: userData[0].email,
+            phone: newNPhoneNumber,
+            name: newNName,
+            cards: userData[0].cards,
+            addresses: userData[0].addresses
+        }
+
+        console.log(dataUser);
+
+        dispatch(startSaveUser(dataUser))
+
+        setTimeout(() => {
+            window.location.reload()
+        }, 500)
+    }
+
+    const handleAInputClick = e => {
+        if (e.target.textContent === 'Editar') {
+            setAInput(true)
+        } else {
+            setAInput(false)
+        }
+    }
+
+    const handleModalOpen = e => {
+
+        modalName = e.target.textContent
+
+        console.log(modalName);
+
+        onOpen()
+    }
+
+    console.log(userData[0].cards);
 
     return (
 
@@ -80,13 +142,13 @@ const DatesProfile = () => {
                     <Typography className={classes.heading}>Datos de la cuenta</Typography>
                 </AccordionSummary>
                 <StyledAccordionDetails>
-                    <StyledTextField id="outlined-basic" type="text" value={userData[0].nickName} label='NickName' variant="outlined" InputProps={{
+                    <StyledTextField type="text" value={userData[0].nickName} label='NickName' variant="outlined" InputProps={{
                         readOnly: true,
                     }} />
-                    <StyledTextField id="outlined-basic" type="email" value={userData[0].email} label='Email' variant="outlined" InputProps={{
+                    <StyledTextField type="email" value={userData[0].email} label='Email' variant="outlined" InputProps={{
                         readOnly: true,
                     }} />
-                    {/* <StyledTextField type="password" value="123456789" id="outlined-basic" label='Password' variant="outlined" InputProps={{
+                    {/* <StyledTextField type="password" value="123456789" label='Password' variant="outlined" InputProps={{
                         readOnly: true,
                     }} /> */}
                 </StyledAccordionDetails>
@@ -99,19 +161,46 @@ const DatesProfile = () => {
                 >
                     <h3 className={classes.heading}>Datos personales</h3>
                 </AccordionSummary>
-                <StyledAccordionDetails>
-                    <StyledTextField id="outlined-basic" value={userData[0].name} label="Name" variant="outlined" />
-                    <StyledTextField id="outlined-basic" value={userData[0].lastName} label='LastName' variant="outlined" />
-                    <StyledTextField id="outlined-basic" value={userData[0].document} label='Document' variant="outlined" InputProps={{
-                        readOnly: true,
-                    }} />
-                    <StyledTextField id="outlined-basic" value={userData[0].phone} label='PhoneNumber' variant="outlined" InputProps={{
-                        readOnly: true,
-                    }} />
-                    <StyledTextField id="outlined-basic" value={userData[0].cellPhone} label='PhoneNumber2' variant="outlined" InputProps={{
-                        readOnly: true,
-                    }} />
-                </StyledAccordionDetails>
+                {
+                    !aInput
+                        ?
+                        <StyledAccordionDetails>
+                            <StyledTextField value={userData[0].name} label="Name" variant="outlined" />
+                            <StyledTextField value={userData[0].lastName} label='LastName' variant="outlined" />
+                            <StyledTextField value={userData[0].document} label='Document' variant="outlined" />
+                            <StyledTextField value={userData[0].phone} label='Phone Number' variant="outlined" />
+                            <StyledTextField value={userData[0].cellPhone} label='Cell Phone Number' variant="outlined" />
+                            <Button
+                                variant="outlined" color="primary"
+                                className={classes.button}
+                                onClick={handleAInputClick}
+                            >
+                                Editar
+                            </Button>
+                        </StyledAccordionDetails>
+                        :
+                        <StyledAccordionDetails>
+                            <StyledTextField inputRef={newName} placeholder="Name" variant="outlined" />
+                            <StyledTextField inputRef={newLastName} placeholder='LastName' variant="outlined" />
+                            <StyledTextField inputRef={newDocument} placeholder='Document' variant="outlined" />
+                            <StyledTextField inputRef={newPhoneNumber} placeholder='Phone Number' variant="outlined" />
+                            <StyledTextField inputRef={newPhoneNumber2} placeholder='Cell Phone Number' variant="outlined" />
+                            <Button
+                                variant="outlined" color="primary"
+                                className={classes.button}
+                                onClick={editUser}
+                            >
+                                Actualizar
+                            </Button>
+                            <Button
+                                variant="outlined" color="primary"
+                                className={classes.button}
+                                onClick={handleAInputClick}
+                            >
+                                Volver
+                            </Button>
+                        </StyledAccordionDetails>
+                }
             </Accordion>
             <Accordion>
                 <AccordionSummary
@@ -132,10 +221,11 @@ const DatesProfile = () => {
                             >
                                 {cards.map((card, index) => {
                                     return (
-                                    <Box display="flex" key={index} marginBottom="10px" fontSize="18px">
-                                        <FaCcMastercard style={{ marginRight: "10px", fontSize: "18px" }} /> {card.numCard}
-                                    </Box>
-                                )})}
+                                        <Box display="flex" key={index} marginBottom="10px" fontSize="18px">
+                                            <FaCcMastercard style={{ marginRight: "10px", fontSize: "18px" }} /> {card.numCard}
+                                        </Box>
+                                    )
+                                })}
                             </Grid>
                         </FormControl>
                         <div>
@@ -143,11 +233,10 @@ const DatesProfile = () => {
                                 variant="outlined" color="primary"
                                 className={classes.button}
                                 startIcon={<MdAddCircle />}
-                                onClick={onOpen}
+                                onClick={handleModalOpen}
 
                             >
-                                {/* <CreditCardModal /> */}
-                                Agregar
+                                Add Card
                             </Button>
                         </div>
                     </div>
@@ -170,14 +259,16 @@ const DatesProfile = () => {
                                 justify="space-between"
                                 alignItems="flex-start"
                             >
-                                {address.map(addr => {
-                                    <Box marginBottom="10px" fontSize="18px" border="solid 1px" borderRadius="20px" padding="20px" >
-                                        <p style={{ fontSize: "20px", fontWeight: "600", display: "flex" }}><BsHouseDoor style={{ marginRight: "10px", fontSize: "18px", marginTop: "5px" }} />{addr.type}</p>
-                                        <p style={{ fontSize: "16px" }}>{addr.country} - {addr.city} - {addr.neighborhood} - {addr.address}</p>
-                                        <p style={{ fontSize: "14px" }}>{addr.complement}</p>
-                                    </Box>
+                                {address.map((addr, index) => {
+                                    return (
+                                        <Box key={index} marginBottom="10px" fontSize="18px" border="solid 1px" borderRadius="20px" padding="20px" >
+                                            <p style={{ fontSize: "20px", fontWeight: "600", display: "flex" }}><BsHouseDoor style={{ marginRight: "10px", fontSize: "18px", marginTop: "5px" }} />{addr.typeAddress}</p>
+                                            <p style={{ fontSize: "16px" }}>{addr.country} - {addr.city} - {addr.neighborhood} - {addr.direction}</p>
+                                            <p style={{ fontSize: "14px" }}>{addr.complement}</p>
+                                        </Box>
+                                    )
                                 })}
-                                <Box marginBottom="10px" fontSize="18px" border="solid 1px" borderRadius="20px" padding="20px" >
+                                {/* <Box marginBottom="10px" fontSize="18px" border="solid 1px" borderRadius="20px" padding="20px" >
                                     <p style={{ fontSize: "20px", fontWeight: "600", display: "flex" }}><BsHouseDoor style={{ marginRight: "10px", fontSize: "18px", marginTop: "5px" }} />Casa</p>
                                     <p style={{ fontSize: "16px" }}>Bogotá - Barrio - Cll 76 CA # 90-26</p>
                                     <p style={{ fontSize: "14px" }}>int 301</p>
@@ -191,7 +282,7 @@ const DatesProfile = () => {
                                     <p style={{ fontSize: "20px", fontWeight: "600", display: "flex" }}><FaMapMarkerAlt style={{ marginRight: "10px", fontSize: "18px", marginTop: "5px" }} />Otro</p>
                                     <p style={{ fontSize: "16px" }}>Bogotá - Barrio - Cll 76 CA # 90-26</p>
                                     <p style={{ fontSize: "14px" }}>int 301</p>
-                                </Box>
+                                </Box> */}
                             </Grid>
                         </FormControl>
                         <div>
@@ -199,8 +290,9 @@ const DatesProfile = () => {
                                 variant="outlined" color="primary"
                                 className={classes.button}
                                 startIcon={<MdAddCircle />}
+                                onClick={handleModalOpen}
                             >
-                                Agregar
+                                Add address
                             </Button>
                         </div>
                     </div>
@@ -210,7 +302,13 @@ const DatesProfile = () => {
                 finalFocusRef={finalRef}
                 isOpen={isOpen}
                 onClose={onClose} >
-                <CreditCardModal onClose={onClose} />
+                {
+                    modalName == 'Add Card'
+                        ?
+                        <CreditCardModal onClose={onClose} />
+                        :
+                        <AddressModal onClose={onClose} />
+                }
             </Modal>
         </div>
 
