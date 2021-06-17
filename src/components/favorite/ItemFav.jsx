56c90de73e15b736013bc8e04b115abb3e88425f
Jonathan Cepeda <jonisjfcp@gmail.com>
2021-06-17 01:59:29 -0500

    haciendo pagina detalles

--- src/components/favorite/ItemFav.jsx
+++ src/components/favorite/ItemFav.jsx
@@ -1,60 +1,67 @@
-import React from 'react';
-import styled from 'styled-components'
-import Button from '@material-ui/core/Button';
-import { useSelector } from 'react-redux';
+import React from "react";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import { useSelector } from "react-redux";
 
 const FavItem = styled.div`
-display:flex;
-justify-content:center;
-background-color:white;
-padding:20px;
-border: 10px double #eaecef;
-@media screen and (max-width: 900px) {
-    flex-wrap:wrap;}
-`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+  padding: 20px;
+  border: 10px double #eaecef;
+  @media screen and (max-width: 900px) {
+    flex-wrap: wrap;
+  }
+`;
 const CenterDiv = styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-width:320px;
-@media screen and (max-width: 900px) {
-   width:auto;      
-}
-
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 320px;
+  @media screen and (max-width: 900px) {
+    width: auto;
+  }
+`;
 
 const ItemFav = (props) => {
+  const { favorite } = useSelector((state) => state.products);
+  console.log(favorite);
+  // const { id, image, nombre, precio } = props
+  return (
+    <>
+      {favorite.map((item, index) => (
+        <FavItem>
+          <div></div>
+          <img src={item.img_url} alt={item.name} />
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            <CenterDiv style={{ flexDirection: "column", textAlign: "center" }}>
+              <h3 style={{ width: "inherit" }}>{item.name}</h3>
+              <span>
+                <h3>{item.price}</h3>
+              </span>
+            </CenterDiv>
+            <CenterDiv style={{ textAlign: "center" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ textAlign: "center", marginBottom: "5px" }}
+              >
+                Add cart
+              </Button>
+            </CenterDiv>
+          </div>
 
-    const { favorite } = useSelector(state => state.products)
-    console.log(favorite);
-    // const { id, image, nombre, precio } = props
-    return (
-        <>
-            {
-                favorite.map((item, index) =>
-                    <FavItem>
-                        <div></div>
-                        <img src={item.img_url} alt={item.name} />
-                        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                            <CenterDiv style={{ flexDirection: 'column', textAlign: 'center' }}>
-                                <h3 style={{ width: 'inherit' }}>{item.name}</h3>
-                                <span><h3>{item.price}</h3></span>
-                            </CenterDiv>
-                            <CenterDiv style={{ textAlign: 'center' }}>
-                                <Button variant="contained" color="primary" style={{ textAlign: 'center' }}>
-                                    Add cart
-                                </Button>
-
-                            </CenterDiv>
-
-                        </div>
-
-                        <hr />
-                    </FavItem>
-                )
-            }
-        </>
-    );
-}
+          <hr />
+        </FavItem>
+      ))}
+    </>
+  );
+};
 
 export default ItemFav;
